aef94d09c57dbf5800297a811ad4edb6ef147cbf
tasuku43 <task.teckac@gmail.com>
2024-01-05 23:22:14 +0900

    install lib

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+			"status":  200,
+		})
+	})
+	router.Run(":8080")
+}
